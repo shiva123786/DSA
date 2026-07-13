66cb91be18ea1acbd00c2aea461bd1251790a0f1
kethavath shiva <ugs23114_aids.shiva@cbit.org.in>
2026-07-14 01:08:12 +0530

    Time: 1 ms (94.5%), Space: 43.5 MB (58.66%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        if(!(str1+str2).equals(str2+str1)){
+            return "";
+        }
+        int g=gcd(str1.length(),str2.length());
+        return str1.substring(0,g);
+        
+    }
+    int gcd(int a,int b){
+        while(b!=0){
+            int t=b;
+            b=a%b;
+            a=t;
+        }
+        return a;
+    }
+}
