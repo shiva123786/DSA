b8a27205e33942499cfcb0ce0b2105b86a917ac5
kethavath shiva <ugs23114_aids.shiva@cbit.org.in>
2026-07-15 23:44:35 +0530

    Time: 0 ms (100%), Space: 19.4 MB (27.3%) - LeetHub

---
+++ 0078-subsets/0078-subsets.py
@@ -0,0 +1,9 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result = [[]]
+        for num in nums:
+            result += [curr + [num] for curr in result]
+        return result
+
+
+        
