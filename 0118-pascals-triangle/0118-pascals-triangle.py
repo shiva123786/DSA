c45b270daac604c56ef295cd707b2d101f0e1be0
kethavath shiva <ugs23114_aids.shiva@cbit.org.in>
2026-07-15 23:47:24 +0530

    Time: 0 ms (100%), Space: 19.4 MB (28.6%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -0,0 +1,10 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        tri=[]
+        for i in range(numRows):
+            row=[1]*(1+i)
+            for j in range(1,i):
+                row[j]=tri[i-1][j-1]+tri[i-1][j]
+            tri.append(row)
+        return tri
+        
