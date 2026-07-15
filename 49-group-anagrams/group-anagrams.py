21c76e571f75246a6b6285011fb4f35e3a6f0844
kethavath shiva <ugs23114_aids.shiva@cbit.org.in>
2026-07-15 17:25:06 +0530

    Time: 11 ms (83.14%) | Memory: 22 MB (79.33%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,7 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        d=defaultdict(list)
+        for st in strs:
+            d["".join(sorted(st))].append(st)
+        return list(d.values())
+        
