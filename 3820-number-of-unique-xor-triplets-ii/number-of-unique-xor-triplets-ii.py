4585b38b3e49d1a12461b0c864ddc379d8f4f0f5
kethavath shiva <ugs23114_aids.shiva@cbit.org.in>
2026-07-24 12:40:12 +0530

    Time: 8702 ms (33.33%) | Memory: 19.7 MB (22.22%) - LeetSync

---
+++ 3820-number-of-unique-xor-triplets-ii/number-of-unique-xor-triplets-ii.py
@@ -0,0 +1,16 @@
+class Solution:
+    def uniqueXorTriplets(self, nums: List[int]) -> int:
+        n=len(nums)
+        if n==1:
+            return 1
+        pairs=set()
+        for i in range(n+1):
+            for j in range(i+1,n):
+                pairs.add(nums[i]^nums[j])
+        sol=set()
+        for x in pairs:
+            for num in nums:
+                sol.add(x^num)
+        return len(sol)
+
+        
