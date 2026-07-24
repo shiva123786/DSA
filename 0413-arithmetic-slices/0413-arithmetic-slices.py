18ae06b8a82940f5df55bb789f8a4761022b055e
kethavath shiva <ugs23114_aids.shiva@cbit.org.in>
2026-07-24 13:55:46 +0530

    Time: 0 ms (100%), Space: 19.4 MB (49.67%) - LeetHub

---
+++ 0413-arithmetic-slices/0413-arithmetic-slices.py
@@ -0,0 +1,12 @@
+class Solution:
+    def numberOfArithmeticSlices(self, nums: List[int]) -> int:
+        sol=0
+        cur=0
+        for i in range(2,len(nums)):
+            if nums[i]-nums[i-1]==nums[i-1]-nums[i-2]:
+                cur+=1
+                sol+=cur
+            else: 
+                cur=0
+        return sol
+        
