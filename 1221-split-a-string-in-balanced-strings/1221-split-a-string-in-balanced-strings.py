52d53fd0ea97d9f6395b8b94f925c954356fa0ad
kethavath shiva <ugs23114_aids.shiva@cbit.org.in>
2026-07-26 15:19:06 +0530

    Time: 0 ms (100%), Space: 19.3 MB (14.24%) - LeetHub

---
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.py
@@ -0,0 +1,12 @@
+class Solution:
+    def balancedStringSplit(self, s: str) -> int:
+        balance=0
+        ans=0
+        for ch in s:
+            if ch=='L':
+                balance+=1
+            else:
+                balance-=1
+            if balance==0:
+                ans+=1
+        return ans
