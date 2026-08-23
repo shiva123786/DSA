b4ab1dc1d57ebbd50aeb74390b425b672400bb93
kethavath shiva <ugs23114_aids.shiva@cbit.org.in>
2026-08-23 20:56:03 +0530

    Time: 0 ms (100%), Space: 19.2 MB (76.78%) - LeetHub

---
+++ 4030-check-ascii-palindromic/4030-check-ascii-palindromic.py
@@ -0,0 +1,6 @@
+class Solution:
+    def isPalindromic(self, s: str) -> bool:
+        binary=""
+        for ch in s:
+            binary+=format(ord(ch),'08b')
+        return binary==binary[::-1]
