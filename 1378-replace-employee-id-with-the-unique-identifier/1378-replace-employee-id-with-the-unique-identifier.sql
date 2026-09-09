2ad50521870a551f04618dcb1f47c98461f7b017
kethavath shiva <ugs23114_aids.shiva@cbit.org.in>
2026-09-09 22:15:12 +0530

    Time: 1784 ms (9.89%), Space: 0B (100%) - LeetHub

--- 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
+++ 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
@@ -1,1 +1,16 @@
 # Write your MySQL query statement below
+SELECT 
+    CASE 
+        WHEN c.cnt > 1 THEN NULL
+        ELSE eu.unique_id
+    END AS unique_id,
+    e.name
+FROM Employees e
+LEFT JOIN EmployeeUNI eu
+    ON e.id = eu.id
+JOIN (
+    SELECT id, COUNT(*) AS cnt
+    FROM Employees
+    GROUP BY id
+) c
+ON e.id = c.id;
